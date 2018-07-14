71875c806ac760da7620ef968b99d40227cae925
nick lian <w19900227@gmail.com>
2018-07-15 01:17:59 +0800

    templaray

--- Sample2/src/Sample2/FileExtensionManager.php
+++ Sample2/src/Sample2/FileExtensionManager.php
@@ -4,7 +4,7 @@ namespace App\Sample2;
 use App\Sample2\IExtensionManager;
 
 class FileExtensionManager implements IExtensionManager {
-	public function Isvalid(string $fileName) {
+	public function Isvalid($fileName) {
 		// to do something
 		// return true;
 	}
